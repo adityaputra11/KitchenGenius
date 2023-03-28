9c84a2c231299f7d4a4fcb70df1fe9a06a588d89
FLP-9-AdityaPratama <adityaputrapratama39@gmail.com.com>
2023-03-29 05:38:32 +0700

    :sparkles: setup navigations

--- app/src/main/java/com/aditya/kitchengenius/MainActivity.kt
+++ app/src/main/java/com/aditya/kitchengenius/MainActivity.kt
@@ -3,6 +3,11 @@ package com.aditya.kitchengenius
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +15,24 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomMenuNav)
+//        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+//            when (menuItem.itemId) {
+//                R.id.menu_home -> {
+//                    // Handle home menu item click
+//                    true
+//                }
+//                R.id.menu_search -> {
+//                    // Handle search menu item click
+//                    true
+//                }
+//                R.id.menu_profile -> {
+//                    // Handle profile menu item click
+//                    true
+//                }
+//                else -> false
+//            }
+//        }
     }
 }
